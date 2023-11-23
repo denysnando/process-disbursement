a48ea4e4c0f4934c92fe8068ba746fcaee61d9c6
denysnando <denysnando@gmail.com>
2023-11-23 08:58:26 -0300

    setup gems rubocop, linters

--- Gemfile
+++ Gemfile
@@ -1,18 +1,17 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
 
-ruby "3.1.2"
-gem "rails", "~> 7.1.1"
+source 'https://rubygems.org'
+
+ruby '3.1.2'
 gem 'pg', '~> 1.3.4'
-gem "puma", ">= 5.0"
-gem "tzinfo-data", platforms: %i[ mswin mswin64 mingw x64_mingw jruby ]
+gem 'puma', '>= 5.0'
+gem 'rails', '~> 7.1.1'
+gem 'redis'
+gem 'sidekiq'
+gem 'tzinfo-data', platforms: %i[mswin mswin64 mingw x64_mingw jruby]
 
 group :development, :test do
-  gem "debug", platforms: %i[ mri mswin mswin64 mingw x64_mingw ]
-  gem 'rubocop', require: false
-  gem 'rubocop-performance', require: false
-  gem 'rubocop-rails', require: false
-  gem 'rubocop-rspec', require: false
-  gem 'solargraph'
+  gem 'debug', platforms: %i[mri mswin mswin64 mingw x64_mingw]
   gem 'factory_bot_rails'
   gem 'faker'
   gem 'pry-rails'
@@ -20,6 +19,17 @@ group :development, :test do
 end
 
 group :development do
-  gem "error_highlight", ">= 0.4.0", platforms: [:ruby]
+  gem 'error_highlight', '>= 0.4.0', platforms: [:ruby]
+  gem 'rubocop', require: false
+  gem 'rubocop-performance', require: false
+  gem 'rubocop-rails', require: false
+  gem 'rubocop-rspec', require: false
+  gem 'solargraph'
 end
 
+group :test do
+  gem 'database_cleaner'
+  gem 'rspec-sidekiq'
+  gem 'shoulda-matchers', '~> 5.0'
+  gem 'simplecov'
+end
--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,10 @@
 require_relative "boot"
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-# require "active_job/railtie"
-require "active_record/railtie"
-# require "active_storage/engine"
-require "action_controller/railtie"
-# require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
-require "action_view/railtie"
-# require "action_cable/engine"
-require "rails/test_unit/railtie"
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
 
 module SequraBackend
   class Application < Rails::Application
@@ -28,6 +16,7 @@ module SequraBackend
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    config.active_job.queue_adapter = :sidekiq
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -37,6 +26,6 @@ module SequraBackend
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    # config.generators.system_tests = nil
   end
 end
