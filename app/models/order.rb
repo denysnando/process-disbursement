05a7928971be53bd1f45091d005a7e06378def3f
denysnando <denysnando@gmail.com>
2023-11-23 11:48:46 -0300

    add logic to calculate fee amounts

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,31 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
+  TIER_1_LIMIT = 50
+  TIER_2_LIMIT = 300
+
+  TIER_1_FEE = 0.01
+  TIER_2_FEE = 0.0095
+  TIER_3_FEE = 0.0085
+
+  # Validations
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+
+  def net_amount
+    amount - calculate_fee_amount.round(2)
+  end
+
+  def calculate_fee_amount
+    amount * fee_percentage
+  end
+
+  def fee_percentage
+    if amount < TIER_1_LIMIT
+      TIER_1_FEE
+    elsif amount < TIER_2_LIMIT
+      TIER_2_FEE
+    else
+      TIER_3_FEE
+    end
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,4 +3,46 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:amount) }
+    it { is_expected.to validate_numericality_of(:amount).is_greater_than(0) }
+  end
+
+  describe '#net_amount' do
+    it 'calculates the net amount correctly' do
+      order = create(:order, amount: 100.0)
+      allow(order).to receive(:calculate_fee_amount).and_return(1.0)
+
+      expect(order.net_amount).to eq(99.0)
+    end
+  end
+
+  describe '#calculate_fee_amount' do
+    it 'calculates the fee amount correctly' do
+      order = build(:order, amount: 100.0)
+      allow(order).to receive(:fee_percentage).and_return(0.01)
+
+      expect(order.calculate_fee_amount).to eq(1.0)
+    end
+  end
+
+  describe '#fee_percentage' do
+    it 'returns the correct fee percentage for Tier 1' do
+      order = build(:order, amount: Order::TIER_1_LIMIT - 1)
+
+      expect(order.fee_percentage).to eq(Order::TIER_1_FEE)
+    end
+
+    it 'returns the correct fee percentage for Tier 2' do
+      order = build(:order, amount: Order::TIER_2_LIMIT - 1)
+
+      expect(order.fee_percentage).to eq(Order::TIER_2_FEE)
+    end
+
+    it 'returns the correct fee percentage for Tier 3' do
+      order = build(:order, amount: Order::TIER_2_LIMIT + 1)
+
+      expect(order.fee_percentage).to eq(Order::TIER_3_FEE)
+    end
+  end
 end
